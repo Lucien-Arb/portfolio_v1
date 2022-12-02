7b6cb2c6cf093ac3ce166acd8c617d26e891ee77
Arbieu Lucien <68686033+Lucien34000@users.noreply.github.com>
2022-12-02 23:39:37 +0100

    correction lien pdf cv pour la prod

--- src/components/menu.js
+++ src/components/menu.js
@@ -266,7 +266,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="/portfolio_v1/resume.pdf" className="resume-link">
               CV
             </a>
           </nav>
